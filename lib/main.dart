3137c997d5f27a416242ead6afa652ffe33a5100
Felipe Sales <soufeliposales@gmail.com>
2021-09-12 17:26:00 -0300

    adding the home feature of the get package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+
+import 'modules/home/home_bindings.dart';
+import 'modules/home/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,58 +13,19 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'Using Dio',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Container(),
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const HomePage(),
+          binding: HomeBindings(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      ],
     );
   }
 }
---
+++ lib/modules/home/home_bindings.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'home_controller.dart';
+
+class HomeBindings implements Bindings {
+  @override
+  void dependencies() {
+    Get.put(
+      HomeController(),
+    );
+  }
+}
