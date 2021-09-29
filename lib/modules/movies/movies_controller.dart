674e7a97fe367e78387391978923f638768ae38a
Felipe Sales <soufeliposales@gmail.com>
2021-09-28 22:42:37 -0300

    basic refactor and adding the hasError getter with an component

--- lib/modules/movies/movies_controller.dart
+++ lib/modules/movies/movies_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 
 import 'package:using_dio/models/movie.dart';
+import 'package:using_dio/models/movies.dart';
 import 'package:using_dio/repositories/movies/movies_repository.dart';
 
 class MoviesController extends GetxController {
@@ -8,10 +9,13 @@ class MoviesController extends GetxController {
   final _totalPages = 0.obs;
   final _movies = <Movie>[].obs;
   final _loading = false.obs;
+  final _error = ''.obs;
 
   int get totalPages => _totalPages.value;
   List<Movie> get movies => _movies;
   bool get loading => _loading.value;
+  bool get hasError => _error.isNotEmpty;
+  String get error => _error.value;
 
   @override
   Future<void> onInit() async {
@@ -20,19 +24,24 @@ class MoviesController extends GetxController {
   }
 
   Future<void> findMovies() async {
-    _loading.toggle();
-    await 3.seconds.delay();
-    MoviesRepository repository = Get.find();
-    if (type == 'populares') {
-      final movies = await repository.findPopularMovies();
+    try {
+      _loading.toggle();
+      _error('');
+      MoviesRepository repository = Get.find();
+      Movies movies;
+
+      if (type == 'populares') {
+        movies = await repository.findPopularMovies();
+      } else {
+        movies = await repository.findTopRatedMovies();
+      }
+
       _totalPages(movies.totalPages);
       _movies.assignAll(movies.results);
       _loading.toggle();
-    } else {
-      final movies = await repository.findTopRatedMovies();
-      _totalPages(movies.totalPages);
-      _movies.assignAll(movies.results);
+    } catch (e) {
       _loading.toggle();
+      _error('Erro ao buscar dados');
     }
   }
 }
--- lib/modules/movies/movies_page.dart
+++ lib/modules/movies/movies_page.dart
@@ -26,68 +26,91 @@ class MoviesPage extends GetView<MoviesController> {
               height: Get.height,
               child: const CircularProgressIndicator(),
             ),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  Container(),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Obx(
-                      () => Text('Films Total: ${controller.totalPages}'),
+            child: Visibility(
+              visible: controller.hasError,
+              // ignore: prefer_const_constructors
+              replacement: _MoviesContent(),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(controller.error),
+                    TextButton(
+                      onPressed: () => controller.findMovies(),
+                      child: const Text('Tentar novamente'),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  Obx(
-                    () => Wrap(
-                      spacing: 10,
-                      alignment: WrapAlignment.center,
-                      children: [
-                        ...controller.movies.map(
-                          (films) {
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    height: 150,
-                                    child: Image.network(
-                                      'https://image.tmdb.org/t/p/w500/${films.posterImageUrl}',
-                                      fit: BoxFit.contain,
-                                    ),
-                                  ),
-                                  Container(
-                                    constraints:
-                                        const BoxConstraints(maxWidth: 150),
-                                    child: Text(
-                                      films.title,
-                                      style:
-                                          Theme.of(context).textTheme.subtitle2,
-                                    ),
-                                  ),
-                                  Container(
-                                    constraints:
-                                        const BoxConstraints(maxWidth: 150),
-                                    child: Text(
-                                      films.overview,
-                                      style:
-                                          Theme.of(context).textTheme.bodyText2,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        ).toList(),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _MoviesContent extends GetView<MoviesController> {
+  const _MoviesContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          Container(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Obx(
+              () => Text('Films Total: ${controller.totalPages}'),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Obx(
+            () => Wrap(
+              spacing: 10,
+              alignment: WrapAlignment.center,
+              children: [
+                ...controller.movies.map(
+                  (films) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 150,
+                            child: Image.network(
+                              'https://image.tmdb.org/t/p/w500/${films.posterImageUrl}',
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          Container(
+                            constraints: const BoxConstraints(maxWidth: 150),
+                            child: Text(
+                              films.title,
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                          ),
+                          Container(
+                            constraints: const BoxConstraints(maxWidth: 150),
+                            child: Text(
+                              films.overview,
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ).toList(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
